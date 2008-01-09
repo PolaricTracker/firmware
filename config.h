71844ecc2a2e99423658db5474bc0204f390f8a9
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2008-01-09 09:56:38 +0000

    set/clear bit macros

--- config.h
+++ config.h
@@ -12,5 +12,17 @@
 #define FBUF_SLOTS    32
 #define FBUF_SLOTSIZE 16
 
-#define DCD_LED_ON  { PORTB |= (1 << PB0); }
-#define DCD_LED_OFF { PORTB &= ~(1 << PB0); }
+#define DCD_LED_PORT PORTB
+#define DCD_LED_BIT  PB0
+
+#define ADF_SCLK_PORT  PORTB
+#define ADF_SCLK_BIT   PB0
+#define ADF_SDATA_PORT PORTB
+#define ADF_SDATA_BIT  PB1
+
+#define _SET_BIT(p, bit)  (p) |= (1 << (bit)) 
+#define _CLR_BIT(p, bit)  (p) &= ~(1 << (bit)) 
+#define SET_BIT(x)       _SET_BIT(x##_PORT, x##_BIT)
+#define CLR_BIT(x)       _CLR_BIT(x##_PORT, x##_BIT)
+
+
