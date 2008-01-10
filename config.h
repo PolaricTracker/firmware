cc34b457fb92bae0676eb53683e1d75e9105d7d3
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2008-01-10 10:11:05 +0000

    Add definitions / macros

--- config.h
+++ config.h
@@ -1,8 +1,9 @@
+#include <stdint.h>
 
 #define TRUE   (1)
 #define FALSE  (0)
 
-#define F_CPU 14745600UL 
+#define F_CPU 8000000UL 
 
 #define	UART_BUF_SIZE	(16)		
 
@@ -12,17 +13,31 @@
 #define FBUF_SLOTS    32
 #define FBUF_SLOTSIZE 16
 
-#define DCD_LED_PORT PORTB
-#define DCD_LED_BIT  PB0
-
-#define ADF_SCLK_PORT  PORTB
-#define ADF_SCLK_BIT   PB0
-#define ADF_SDATA_PORT PORTB
-#define ADF_SDATA_BIT  PB1
-
-#define _SET_BIT(p, bit)  (p) |= (1 << (bit)) 
-#define _CLR_BIT(p, bit)  (p) &= ~(1 << (bit)) 
-#define SET_BIT(x)       _SET_BIT(x##_PORT, x##_BIT)
-#define CLR_BIT(x)       _CLR_BIT(x##_PORT, x##_BIT)
+#define DCD_LED_PORT    PORTD
+#define DCD_LED_BIT     PD5
+#define ADF_SCLK_PORT   PORTB
+#define ADF_SCLK_BIT    PB0
+#define ADF_SDATA_PORT  PORTB
+#define AFD_SDATA_BIT   PB1
 
 
+/*
+ * These are specific to the USB KEY.  
+ */
+#define USBKEY_LED1_PORT   PORTD
+#define USBKEY_LED1_BIT    PD4
+#define USBKEY_LED2_PORT   PORTD
+#define USBKEY_LED2_BIT    PD5
+#define USBKEY_LED3_PORT   PORTD
+#define USBKEY_LED3_BIT    PD6
+#define USBKEY_LED4_PORT   PORTD
+#define USBKEY_LED4_BIT    PD7
+
+
+/*
+ * Bit fiddling macros
+ */
+#define bit_is_set(x)  (x##_PORT) & (x##_BIT)
+#define set_bit(x)     (x##_PORT) |= (1  << (x##_BIT))
+#define clear_bit(x)   (x##_PORT)  &= ~(1 << x##_BIT)
+#define toggle_bit(x)  (x##_PORT) ^= (x##_BIT)
