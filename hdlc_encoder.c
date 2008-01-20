1d9e32c274597ff373dcade2dbb70605b3cf47e1
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2008-01-20 16:40:43 +0000

    Rewrite hdlc_encode_frames

--- hdlc_encoder.c
+++ hdlc_encoder.c
@@ -31,22 +31,22 @@ static uint8_t testbyte;
 enum {TXOFF, TXPRE, TXPACKET, TXPOST} txState; 
 
 /* FIXME: Those should be parameters stored in EEPROM ! */
-#define TXDELAY 30
-#define TXTAIL 4
+#define TXDELAY 60
+#define TXTAIL 40
 #define PERSISTENCE 100
 #define SLOTTIME    50
 
 static void hdlc_txencoder();
 static void hdlc_testsignal();
 static void hdlc_encode_frames();
-
+static void hdlc_encode_byte(uint8_t txbyte, bool flag);
 
    
 fbq_t* hdlc_init_encoder(stream_t* os)
 {
    outstream = os;
-   DEFINE_FBQ(encoder_queue, HDLC_ENCODER_QUEUE_SIZE); 
-   THREAD_START( hdlc_txencoder, DEFAULT_STACK_SIZE);
+   FBQ_INIT( encoder_queue, HDLC_ENCODER_QUEUE_SIZE ); 
+   THREAD_START( hdlc_txencoder, DEFAULT_STACK_SIZE );
    
    sem_init(&test, 0);
    THREAD_START( hdlc_testsignal, DEFAULT_STACK_SIZE);
@@ -94,27 +94,30 @@ static void hdlc_testsignal()
  *******************************************************************************/
  
 static void hdlc_txencoder()
-{  
-   /* Get frame from buffer-queue when available. This is a blocking call.
-    * FIXME: Protect against concurrent access
-    */
-   buffer = fbq_get(&encoder_queue);     
-       
-   /* Wait until channel is free 
-    * P-persistence algorithm 
-    */
-   for (;;) {
-     while ( !afsk_channel_ready(0) )  // FIXME
-        t_yield(); 
-     int r  = rand(); 
-     if (r > PERSISTENCE * 255)
-         sleep(SLOTTIME); 
-     else
-         break;
-   }
+{ 
+   while (true)  
+   {
+      /* Get frame from buffer-queue when available. 
+       * This is a blocking call.
+       */     
+      buffer = fbq_get(&encoder_queue);    
+
+      /* Wait until channel is free 
+       * P-persistence algorithm 
+       */
+      for (;;) {
+        while ( !afsk_channel_ready(0) )  // FIXME
+           t_yield(); 
+        int r  = rand(); 
+        if (r > PERSISTENCE * 255)
+            sleep(SLOTTIME); 
+        else
+            break;
+      }
   
-   afsk_ptt_on();                      // FIXME
-   hdlc_encode_frames();
+      afsk_ptt_on();                      // FIXME
+      hdlc_encode_frames();
+   }
 }
    
 
@@ -128,77 +131,52 @@ static void hdlc_txencoder()
  
 static void hdlc_encode_frames()
 {
-    uint8_t  bit_zero, txbyte=0, txbits=0, flag=0, nflags=0;  
-    uint8_t outbyte=0, outbits=0, crc=0;
-    uint8_t sequential_ones=0;
-    
-    txState = TXPRE;
-    txbytes = 0;
-    while (TRUE) {
-       /* 
-        * This part (if txbits == 0) determines the next byte to send
-        */  
-       if (txbits == 0) 
-       {
-           /* 
-            * State machine: 
-            * TXPRE    - start sending frame - send as many flags as indicated in TXDELAY
-            * TXPOST   - end frame - send as many flags as indicated in TXTAIL
-            * TXPACKET - send frame data until buffer is empty.         
-            */
-           txbits = 8;  
-           if (txState == TXPRE && nflags >= TXDELAY)
-               txState = TXPACKET;
-             
-           else if (txState == TXPOST && nflags >= TXTAIL) {
-               nflags = 0; 
-               return;
-           }
-           else if (txState == TXPACKET && BUFFER_EMPTY) {
-               nflags = 0;                               // If end of buffer, go to TXPOST state (send end-of-frame flags)
-               txState = TXPOST;                         // FIXME: Allow more than one frame per transmission?
-               fbuf_release(&buffer);
-               txbyte = crc;                             // Insert checksum
-               break;                                   
-           }    
-     
-           /*
-            * Do the work (depending on the state). 
-            * Get next byte to transmit, or use FLAG
-            */
-           if (txState == TXPACKET) {          
-               txbyte = fbuf_getChar(&buffer);            // send next byte from buffer
-               crc = _crc_ccitt_update (crc, txbyte);     // compute checksum
-               txbytes++; 
-               flag = 0;        
-           }
-           else {                                 
-               flag = txbyte = HDLC_FLAG;                 // Send flag. And indicate that this was done deliberately
-               nflags++;                                  // And count how many flags was sent  
-           }
-       }
-    
-    
-       /* 
-        * Transmit the bits with bit stuffing.
-        */
-       if (flag)
-           sequential_ones = 0; 
-        
-       bit_zero = txbyte & 0x01;  
-       if (!bit_zero || sequential_ones++ == 6) 
-           sequential_ones = 0;
+     uint16_t crc = 0;
+     uint8_t txbyte, i;
+   
+     for (i=0; i<TXDELAY; i++)
+         hdlc_encode_byte(HDLC_FLAG, true);
 
-       if (sequential_ones < 6) 
-          { txbyte >>= 1; txbits--; }    
-       
-       outbyte >>= 1;
-       outbyte |= (bit_zero << 7); 
-       outbits++;
-       if (!sequential_ones)                       
-          { outbyte >>= 1; outbits++; }  /* Bit stuffing */ 
-       
-       putch(outstream, outbyte);       
-    }
+     while(!BUFFER_EMPTY)
+     {
+         txbyte = fbuf_getChar(&buffer);
+         crc = _crc_ccitt_update (crc, txbyte);
+         hdlc_encode_byte(txbyte, false);
+     }
+     hdlc_encode_byte(crc, false);
+    
+     for (i=0; i<TXTAIL; i++)
+         hdlc_encode_byte(HDLC_FLAG, true);
+}
+
+
+
+
+
+static void hdlc_encode_byte(uint8_t txbyte, bool flag)
+{    
+     static uint8_t outbits = 0;
+     static uint8_t sequential_ones = 0;
+     static uint8_t outbyte;
+     
+     for (uint8_t i=1; i<8+1; i++)
+     { 
+        register uint8_t bit_zero = txbyte & 0x01;
+        if (!flag && bit_zero && sequential_ones++ == 6) {
+            i--;
+            sequential_ones = 0;     
+        }
+        else {
+            txbyte >>= 1;       
+            outbyte |= (bit_zero << 7); 
+        }
+     
+        if (outbits++ == 8) {
+            putch(outstream, outbyte);
+            outbits = 0;
+        }
+        else
+            outbyte >>= 1;    
+     }   
 }
 
