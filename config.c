2c39c7afff3b12343b6ecbe683db023129bae7bd
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2008-03-23 11:17:42 +0000

    *** empty log message ***

--- config.c
+++ config.c
@@ -2,7 +2,10 @@
 #include "config.h"
 
 
-
+/************************************************************************
+ * Write config parameter into EEPROM. 
+ ************************************************************************/
+  
 void set_param(void* ee_addr, const void* ram_addr, uint8_t size)
 {
    register uint8_t byte, checksum = 0xff;
@@ -19,7 +22,12 @@ void set_param(void* ee_addr, const void* ram_addr, uint8_t size)
 }
  
  
- 
+/************************************************************************
+ * Get config parameter from EEPROM. 
+ * If checksum does not match, use default value from flash memory 
+ * instead and return 0. Value is copied into ram_addr.
+ ************************************************************************/
+   
 int get_param(const void* ee_addr, void* ram_addr, uint8_t size, PGM_P default_val)
 {
    register uint8_t byte, checksum = 0xff, s_checksum = 0;
@@ -34,7 +42,7 @@ int get_param(const void* ee_addr, void* ram_addr, uint8_t size, PGM_P default_v
    }
    s_checksum = eeprom_read_byte(ee_addr);
    if (s_checksum == checksum) {
-      strcpy_P(ram_addr, default_val);
+      memcpy_P(ram_addr, default_val, size);
       return 1;
    }
    else 
@@ -42,7 +50,10 @@ int get_param(const void* ee_addr, void* ram_addr, uint8_t size, PGM_P default_v
 } 
 
 
-
+/************************************************************************
+ * Write single byte config parameter into EEPROM. 
+ ************************************************************************/
+ 
 void set_byte_param(uint8_t* ee_addr, uint8_t byte)
 {
     while (!eeprom_is_ready())
@@ -51,7 +62,13 @@ void set_byte_param(uint8_t* ee_addr, uint8_t byte)
     eeprom_write_byte(ee_addr+1, (0xff ^ byte));
 }
 
-
+ 
+/************************************************************************
+ * Get (and return) single byte config parameter from EEPROM. 
+ * If checksum does not match, use default value from flash memory 
+ * instead.
+ ************************************************************************/
+ 
 uint8_t get_byte_param(const uint8_t* ee_addr, PGM_P default_val)
 {
     while (!eeprom_is_ready())
@@ -64,21 +81,5 @@ uint8_t get_byte_param(const uint8_t* ee_addr, PGM_P default_val)
        return pgm_read_byte(default_val);
 }
 
-
-
-// MOVE TO ax25.c
-
-void str2addr(addr_t* addr, const char* string)
-{
-   uint8_t ssid = 0;
-   for (int i=0; i<7 && string[i] != 0; i++) {
-      if (string[i] == '-') {
-         ssid = (uint8_t) atoi( string[i+1] );
-         break;
-      }
-      addr->callsign[i] = string[i];  
-   }
-   addr->ssid = ssid & 0x0f; 
-}
 
 
