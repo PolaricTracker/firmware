dde7e21be6357615b98bbf50c7fa0ed6112d3f33
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2008-03-26 22:09:49 +0000

    More macros

--- defines.h
+++ defines.h
@@ -20,20 +20,36 @@
 
 #define DCD_LED_PORT    PORTD
 #define DCD_LED_BIT     PD5
-#define ADF_SCLK_PORT   PORTC
-#define ADF_SCLK_BIT    PC4
-#define ADF_SDATA_PORT  PORTC
-#define ADF_SDATA_BIT   PC2
-#define ADF_SREAD_PORT  PORTC
-#define ADF_SREAD_BIT   PC3
 
-#define ADF_TXRXDATA_PORT PORTB
-#define ADF_TXRXDATA_BIT  PB3
+
+/*
+ * ADF 7021 specific definitions
+ */
+#define ADF7021_XTAL        2400000 
+#define ADF7021_SCLK_PORT   PORTC
+#define ADF7021_SCLK_DDR    DDRC
+#define ADF7021_SCLK_BIT    PC4
+#define ADF7021_SDATA_PORT  PORTC
+#define ADF7021_SDATA_DDR   DDRC
+#define ADF7021_SDATA_BIT   PC2
+#define ADF7021_SREAD_PORT  PORTC
+#define ADF7021_SREAD_PORT  DDRC
+#define ADF7021_SREAD_BIT   PC3
+#define ADF7021_SLE_PORT    PORTB
+#define ADF7021_SLE_DDR     DDRB
+#define ADF7021_SLE_BIT     PB3
+#define ADF7021_ON_PORT     PORTC
+#define ADF7021_ON_DDR      DDRC
+#define ADF7021_ON_BIT      PC5
+#define ADF7021_MUXOUT_PORT PORTC
+#define ADF7021_MUXOUT_DDR  DDRC
+#define ADF7021_MUXOUT_BIT  PC5
 
 
 /*
  * These are specific to the USB KEY.  
  */
+  
 #define USBKEY_LED1_PORT   PORTD
 #define USBKEY_LED1_BIT    PD4
 #define USBKEY_LED2_PORT   PORTD
@@ -66,6 +82,21 @@
 #define set_sfr_bit(sfr,bit) (sfr)|=(1<<(bit))
 #define clear_sfr_bit(sfr,bit) (sfr)&=~(1<<(bit))
 #define toggle_sfr_bit(sfr,bit) (sfr)^=(1<<(bit));
+
+#define set_ddr(x)     (x##_DDR) |= _BV(x##_BIT)
+#define clear_ddr(x)   (x##_DDR) &= ~_BV(x##_BIT)
+#define set_port(x)    (x##_PORT) |= _BV(x##_BIT)
+#define clear_port(x)  (x##_PORT) &= ~_BV(x##_BIT)
+#define port_is_set(x) (x##_PORT & _BV(x##_BIT))
+#define toggle_port(x) (x##_PIN) |= _BV(x##_BIT) 
+#define pin_is_high(x) (x##_PIN & _BV(x##_BIT))
+
+/* Use float (or tri-state as Atmel calls it) if pin has external
+   pull-up. Be carefull when switching between input and output or
+   output and input. */
+#define make_output(x) set_ddr(x)
+#define make_input(x) { clear_ddr(x); set_port(x); }
+#define make_float(x) { clear_ddr(x); clear_port(x); }
 
 /*
  * other
