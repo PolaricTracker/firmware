102df57158bbec87e6f96ea1631c9911a4b746a1
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2008-05-06 08:59:28 +0000

    Add more

--- config.h
+++ config.h
@@ -30,12 +30,16 @@ typedef addr_t __digilist_t[7];
  *            Name     Type     
  ***************************************************************/ 
            
-DEFINE_PARAM( MYCALL,  addr_t       );
-DEFINE_PARAM( DEST,    addr_t       );
-DEFINE_PARAM( DIGIS,   __digilist_t );
-DEFINE_PARAM( NDIGIS,  uint8_t      );
-DEFINE_PARAM( TXDELAY, uint8_t      );
-DEFINE_PARAM( TXTAIL,  uint8_t      );
+DEFINE_PARAM( MYCALL,       addr_t       );
+DEFINE_PARAM( DEST,         addr_t       );
+DEFINE_PARAM( DIGIS,        __digilist_t );
+DEFINE_PARAM( NDIGIS,       uint8_t      );
+DEFINE_PARAM( TXDELAY,      uint8_t      );
+DEFINE_PARAM( TXTAIL,       uint8_t      );
+DEFINE_PARAM( TRX_FREQ,     uint32_t     );
+DEFINE_PARAM( TRX_TXPOWER,  double        );
+DEFINE_PARAM( TRX_AFSK_DEV, uint16_t     );
+
 
 
 #if defined __CONFIG_C__
@@ -45,12 +49,15 @@ DEFINE_PARAM( TXTAIL,  uint8_t      );
  * or the linker will complain.
  ***************************************************************/
 
-DEFAULT_PARAM(MYCALL, addr_t)       = {"NOCALL",0};
-DEFAULT_PARAM(DEST, addr_t)         = {"NONE", 0};
-DEFAULT_PARAM(DIGIS, __digilist_t)  = {};
-DEFAULT_PARAM( NDIGIS,  uint8_t)    = 0;
-DEFAULT_PARAM( TXDELAY, uint8_t)    = 20;
-DEFAULT_PARAM( TXTAIL,  uint8_t)    = 10;
+DEFAULT_PARAM( MYCALL, addr_t)         = {"NOCALL",0};
+DEFAULT_PARAM( DEST, addr_t)           = {"NONE", 0};
+DEFAULT_PARAM( DIGIS, __digilist_t)    = {};
+DEFAULT_PARAM( NDIGIS,  uint8_t)       = 0;
+DEFAULT_PARAM( TXDELAY, uint8_t)       = 20;
+DEFAULT_PARAM( TXTAIL,  uint8_t)       = 10;
+DEFAULT_PARAM( TRX_FREQ, uint32_t)     = 144.800e6;
+DEFAULT_PARAM( TRX_TXPOWER, double)    = -13.0;
+DEFAULT_PARAM( TRX_AFSK_DEV, uint16_t) = 3500;
 
 #endif
  
