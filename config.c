585657d7ca0492ed99c415fed103774a3bb80bbb
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2008-08-23 00:02:46 +0000

    Compatible checksumming

--- config.c
+++ config.c
@@ -60,7 +60,7 @@ void set_byte_param(uint8_t* ee_addr, uint8_t byte)
     while (!eeprom_is_ready())
       t_yield();
     eeprom_write_byte(ee_addr, byte);
-    eeprom_write_byte(ee_addr+1, (0xff ^ byte));
+    eeprom_write_byte(ee_addr+1, (0x0f ^ byte));
 }
 
  
@@ -76,7 +76,7 @@ uint8_t get_byte_param(const uint8_t* ee_addr, PGM_P default_val)
       t_yield();
     register uint8_t b1 = eeprom_read_byte(ee_addr);
     register uint8_t b2 = eeprom_read_byte(ee_addr+1);
-    if ((0xff ^ b1) == b2)
+    if ((0x0f ^ b1) == b2)
        return b1;
     else
        return pgm_read_byte(default_val);
