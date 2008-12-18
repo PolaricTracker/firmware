5ec38517d4970792ccbb6313a891735f283c5877
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2008-12-18 21:15:06 +0000

    *** empty log message ***

--- ui.h
+++ ui.h
@@ -7,8 +7,12 @@ void beep(uint16_t);
 void led_init(void);
 void led_usb_on(void);
 void led_usb_off(void);
+void led_usb_restore(void);
 void rgb_led_on(bool, bool, bool);
 void rgb_led_off(void);
 void batt_extcharger_event(void);
+float batt_voltage(void);
+void turn_off(void);
+void led_usb_restore(void);
 
 #endif
