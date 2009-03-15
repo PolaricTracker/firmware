43dd33d8c5ffa8abbf0f3695bc345bcf9dface85
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2009-03-15 00:15:09 +0000

    Several fixes (now it works)

--- hdlc_decoder.c
+++ hdlc_decoder.c
@@ -9,6 +9,7 @@
 #include <avr/pgmspace.h>
 #include "ui.h"
 #include "config.h"
+#include "ax25.h"
 
 
 static stream_t *out; 
@@ -17,6 +18,8 @@ static fbuf_t fbuf;
 static fbq_t fbin;
 
 static void hdlc_decode (void);
+static bool crc_match(FBUF*, uint8_t);
+
 
 
 fbq_t* hdlc_init_decoder (stream_t *s, stream_t *outstr)
@@ -30,19 +33,21 @@ fbq_t* hdlc_init_decoder (stream_t *s, stream_t *outstr)
    return &fbin;
 }
 
+
+
 static uint8_t get_bit ()
 {
    static uint16_t bits = 0xffff;
    static uint8_t bit_count = 0;
   
-   if (bit_count <= 8) {
-     bits |= (uint16_t)getch (stream) << bit_count;
+   if (bit_count < 8) {
+     register uint8_t byte = getch(stream);
+     bits |= ((uint16_t) byte << bit_count);
      bit_count += 8;
    }
-   if ((bits & 0x00ff) == HDLC_FLAG) {
+   if ((uint8_t) (bits & 0x00ff) == HDLC_FLAG) {
      bits >>= 8;
      bit_count -= 8;
-     toggle_port(LED2);
      return HDLC_FLAG;
    } 
    else {
@@ -55,70 +60,89 @@ static uint8_t get_bit ()
 
 
 
+extern uint8_t decode_addr(FBUF *b, addr_t* a);
+
 static void hdlc_decode ()
 {
    uint8_t bit;
-   TRACE(151); 
+
    /* Sync to next flag */
    flag_sync:
-   TRACE(152);
-//   rgb_led_on(true,false,true);
    do {      
       bit = get_bit ();
    }  while (bit != HDLC_FLAG);
-  
+
+   
    /* Sync to next frame */
    frame_sync:
-   TRACE(153);
-//   rgb_led_on(true,true,false);
    do {      
       bit = get_bit ();
    }  while (bit == HDLC_FLAG);
 
-   TRACE(154);
+   
    /* Receiving frame */
    uint8_t bit_count = 0;
    uint8_t ones_count = 0;
-   uint16_t length = 0, crc = 0;
+   uint16_t length = 0;
    uint8_t octet = 0;
   
    fbuf_release (&fbuf); // In case we had an abort or checksum
                          // mismatch on the previous frame
-  
    do {
       if (length > MAX_HDLC_FRAME_SIZE)
          goto flag_sync; // Lost termination flag or only receiving noise?
-//      rgb_led_on(false,true,false);
-      TRACE(155);
+
       for (bit_count = 0; bit_count < 8; bit_count++) 
       {
          octet = (bit ? 0x80 : 0x00) | (octet >> 1);
-         if (bit) ones_count++;    
-         bit = get_bit ();
-         if (bit == HDLC_FLAG)   // Not very likely, but could happen
+         if (bit) ones_count++;   
+         else ones_count = 0; 
+         if (bit == HDLC_FLAG)    // Not very likely, but could happen
             goto frame_sync;
          
+         bit = get_bit ();
+
          if (ones_count == 5) {
-            TRACE(156);
-            if (bit)             // Got more than five consecutive one bits,
-               goto flag_sync;   // which is a certain error
+            if (bit)              // Got more than five consecutive one bits,
+               goto flag_sync;    // which is a certain error
             ones_count = 0;
-            bit = get_bit();     // Bit stuffing - skip one bit
+            bit = get_bit();      // Bit stuffing - skip one bit
          }
       }
-      crc = _crc_ccitt_update (crc, octet);
       fbuf_putChar(&fbuf, octet);
       length++;
-      TRACE(157);
    } while (bit != HDLC_FLAG);
-   
-   TRACE(158);
-   rgb_led_on(false,true,false);
-   if (true || crc == 0xf0b8) {
-      putstr_P(out, PSTR("*** RECEIVED PACKET\r\n"));
+
+
+   sleep(5);   
+   if (crc_match(&fbuf, length)) {
+      /* Display frame */
+      ax25_display_frame(out, &fbuf);
+      putstr_P(out, PSTR("\r\n"));
+      fbuf_release(&fbuf);
       // fbq_put(&fbin, fbuf);       
-      fbuf_new(&fbuf);
+      // fbuf_new(&fbuf);
    }
   
    goto frame_sync; // Two consecutive frames may share the same flag
+}
+
+
+
+
+bool crc_match(FBUF* b, uint8_t length)
+{
+   /* Calculate frame from frame content */
+   uint16_t crc = 0xFFFF;
+   uint8_t i;
+   for (i=0; i<length-2; i++)
+       crc = _crc_ccitt_update(crc, fbuf_getChar(b)); 
+       
+   /* Get CRC from 2 last bytes of frame */
+   uint16_t rcrc; 
+   fbuf_rseek(b, length-2);
+   rcrc = (uint16_t) fbuf_getChar(b) ^ 0xFF;
+   rcrc |= (uint16_t) (fbuf_getChar(b) ^ 0xFF) << 8;
+ 
+   return (crc==rcrc);
 }
