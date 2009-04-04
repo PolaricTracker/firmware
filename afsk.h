3152a219b02f5c29e4e82ac7a41e7fc43aa2ab22
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2009-04-04 20:41:22 +0000

    comments

--- afsk.h
+++ afsk.h
@@ -1,5 +1,5 @@
 /*
- * $Id: afsk.h,v 1.9 2009-01-17 11:32:23 la7eca Exp $
+ * $Id: afsk.h,v 1.10 2009-04-04 20:41:22 la7eca Exp $
  * Some common definitions for AFSK modem
  */
  
@@ -18,27 +18,28 @@ enum {
   SPACE      // 2200 Hz
 };
 
-
 #define AFSK_TXTONE_MARK  1200
 #define AFSK_TXTONE_SPACE 2200
 #define AFSK_BAUD 1200
 
 
+/* Operations for AFSK modulator/transmitter */
 stream_t* afsk_init_encoder(void);
 void afsk_ptt_on(void);
 void afsk_ptt_off(void);
 void afsk_txBitClock(void);
 void afsk_high_tone(bool t);
 
+
+/* Operations for AFSK demodulator/receiver */
 stream_t* afsk_init_decoder (void);
 void afsk_enable_decoder (void);
 void afsk_disable_decoder (void);
 bool afsk_channel_ready (uint16_t); /* ms, µs, something else? */
 void afsk_check_channel ();
 
-extern bool transmit; 
-extern uint8_t dcd;
 
+/* Packet queues (outgoing, incoming) */
 extern FBQ outbuf, fbin; 
 
 
