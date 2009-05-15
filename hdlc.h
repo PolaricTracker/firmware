0fbc2c797c243415bf5950f66fd36932c1dd5d0e
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2009-05-15 22:51:43 +0000

    Packet monitoring...

--- hdlc.h
+++ hdlc.h
@@ -8,13 +8,19 @@
 #define MAX_HDLC_FRAME_SIZE 289 // including FCS field
 
 
-fbq_t* hdlc_init_decoder (stream_t *, stream_t *);
+fbq_t* hdlc_init_decoder (stream_t *);
 fbq_t* hdlc_init_encoder (stream_t *);
 
+void hdlc_monitor_rx(fbq_t*);
+void hdlc_monitor_tx(fbq_t*);
+void hdlc_subscribe(fbq_t*);
 void hdlc_test_on(uint8_t);
 void hdlc_test_off(void);
 void hdlc_wait_idle(void);
 bool hdlc_enc_packets_waiting(void);
 
+/* Packet monitoring (defined in monitor.c) */
+void mon_init(stream_t*);
+void mon_activate(bool);
 
 #endif /* __HDLC_H__ */
