fba0c387e7f18ab9802da27173c9f8e802092c31
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2009-06-25 21:30:32 +0000

    Move transceiver setup to radio.c

--- main.c
+++ main.c
@@ -94,45 +94,6 @@ void usbSerListener(void)
 
 
 
-adf7021_setup_t trx_setup;
-
-/**************************************************************************
- * Setup the adf7021 tranceiver.
- *   - We may move this to a separate source file or to config.c ?
- *   - Parts of the setup may be stored in EEPROM?
- **************************************************************************/
-void setup_transceiver(void)
-{
-    uint32_t freq; 
-    int16_t  fcal;
-    double power; 
-    uint16_t dev;
-    uint16_t afc;
-    
-    GET_PARAM(TRX_FREQ, &freq);
-    GET_PARAM(TRX_CALIBRATE, &fcal);
-    GET_PARAM(TRX_TXPOWER, &power);
-    GET_PARAM(TRX_AFSK_DEV, &dev);
-    GET_PARAM(TRX_AFC, &afc);
-    
-    adf7021_setup_init(&trx_setup);
-    adf7021_set_frequency (&trx_setup, freq+fcal);
-    trx_setup.vco_osc.xosc_enable = true;
-    trx_setup.test_mode.analog = ADF7021_ANALOG_TEST_MODE_RSSI;
-    
-    adf7021_set_data_rate (&trx_setup, 4400);    
-    adf7021_set_modulation (&trx_setup, ADF7021_MODULATION_OVERSAMPLED_2FSK, dev);
-    adf7021_set_power (&trx_setup, power, ADF7021_PA_RAMP_OFF);   
-    adf7021_set_demodulation (&trx_setup, ADF7021_DEMOD_2FSK_LINEAR);
-    adf7021_enable_AFC(&trx_setup, afc);
-    trx_setup.demod.if_bw = ADF7021_DEMOD_IF_BW_12_5;
-    adf7021_set_post_demod_filter (&trx_setup, 3500); 
-    ADF7021_INIT_REGISTER(trx_setup.test_mode, ADF7021_TEST_MODE_REGISTER);
-    trx_setup.test_mode.rx = ADF7021_RX_TEST_MODE_LINEAR_SLICER_ON_TxRxDATA;
-
-    adf7021_init (&trx_setup);
-}
-
 void reset_params()
 {
     /* If version was changed */
@@ -142,6 +103,7 @@ void reset_params()
         SET_BYTE_PARAM(VERSION_KEY, CURRENT_VERSION_KEY);
     }
 }
+
 
 
 /**************************************************************************
