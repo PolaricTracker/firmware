16d46f4be2e6865e97bbe4721895b10c67c161f5
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2009-07-23 20:40:29 +0000

    Run thread only when used

--- monitor.c
+++ monitor.c
@@ -21,29 +21,41 @@ void mon_init(stream_t* outstr)
     out = outstr;
     bcond_init(&mon_ok, true);
     FBQ_INIT(mon, HDLC_DECODER_QUEUE_SIZE);
-    THREAD_START(mon_thread, STACK_MONITOR);
 }
 
 
 void mon_activate(bool m)
 { 
+   bool tstart = m && !mon_on;
+   bool tstop = !m && mon_on;
+   
    mon_on = m;
    FBQ* mq = (mon_on? &mon : NULL);
    hdlc_monitor_rx(mq);
    if (!mon_on || GET_BYTE_PARAM(TXMON_ON))
       hdlc_monitor_tx(mq);
+   if (tstart) 
+      THREAD_START(mon_thread, STACK_MONITOR);  
+   if (tstop)
+      fbq_signal(&mon);
 }
 
 
 
 static void mon_thread()
 {
-    while (true)
+    while (mon_on)
     {
         /* Wait for frame and then to AFSK decoder/encoder 
-         * is not running 
+         * is not running. 
+         *
+         * FIXME: This thread will not terminate because it waits
+         * on fbq_get. Kill thread the hard way or send something to 
+         * the mon queue?
          */
         FBUF frame = fbq_get(&mon);
+        if (fbuf_empty(&frame))
+            continue;
         bcond_wait(&mon_ok);
         
         /* Display it */
