d568a4262812878eaf04774b701d76264e7503a9
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2009-07-23 10:27:14 +0000

    Free stack when task terminates (now tasks can be created "dynamically"). Interface to get info about number of tasks.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,11 +6,35 @@
 #include "kernel.h"
 #include <avr/interrupt.h>
 #include "defines.h"
+#include "../config.h"
 
-static TCB root_task;         /* root task is assigned the initial thread (main) */
-static TCB * q_head, * q_end; /* Ready queue */
+static TCB root_task;          /* root task is assigned the initial thread (main) */
+static TCB * q_head, * q_end;  /* Ready queue */
+static TCB * fl_head;          /* Ordered list of terminated tasks which have stack space to be freed */
 
+static uint8_t lastpid = 0, terminated = 0;
+static void *stackbase;
 static void *stack; 
+
+static void _free_stack(TCB*);
+
+
+uint16_t t_stackUsed()   { return (uint16_t) (stackbase - stack); }
+uint8_t t_nTasks()       { return lastpid; }
+uint8_t t_nTerminated()  { return terminated; }
+
+uint8_t t_nRunning()    
+{   CONTAINS_CRITICAL;
+    uint8_t n = 1; 
+    enter_critical();
+    TCB* p = q_head; 
+    if (p==NULL)
+       n = 0; 
+    else while (p != q_end)
+       { p=p->next; n++; }
+    leave_critical();
+    return n; 
+}
 
 
 /****************************************************************************
@@ -23,6 +47,7 @@ void init_kernel(uint16_t stsize)
         enter_critical();
         q_head = q_end = &root_task;
         q_head->next = q_head;
+        fl_head = NULL;
         leave_critical(); 
         asm volatile(
             "in __tmp_reg__,  __SP_L__"  "\n\t"
@@ -31,9 +56,10 @@ void init_kernel(uint16_t stsize)
             "mov %B0, __tmp_reg__"       "\n\t" 
              : "=e" (stack) :
          );
-        root_task.stackbase = (uint16_t) stack;
+        root_task.pid = 0;
+        stackbase = stack;
+        root_task.stsize = stsize;
         stack -= stsize; 
-
 }
 
 
@@ -44,15 +70,18 @@ void init_kernel(uint16_t stsize)
  ****************************************************************************/
 
 void _t_start(void (*task)(), TCB * tcb, uint16_t stsize)
-{   CONTAINS_CRITICAL;
+{   CONTAINS_CRITICAL;       
     if (setjmp(q_head->env) == 0)
     {
+        /* Put the TCB into the ready queue */
         enter_critical();
         tcb->next = q_head->next; 
         q_head->next = tcb; 
         q_end = q_head; 
         q_head = tcb; 
-        tcb->stackbase = (uint16_t) stack;
+        tcb->stsize = stsize;
+        lastpid++;
+        tcb->pid = lastpid;
         leave_critical();
         
         /* Set stack pointer and call thread function */
@@ -64,23 +93,70 @@ void _t_start(void (*task)(), TCB * tcb, uint16_t stsize)
            ::
             "e" (stack)
          ); 
-         
 
          stack -= stsize; 
          (*task)();
-        
+
         /* Thread is terminated. The last thing to do is to 
          * remove task and switch to the next entry in the 
          * ready queue 
          */
-         
         enter_critical();
+        register TCB* x = q_head;
         q_head = q_head->next;
         q_end->next = q_head;
+        _free_stack(x);
         leave_critical();
         longjmp(q_head->env, 1);
     } 
 }
+
+
+/****************************************************************************
+ * Try to free the space used on the stack by a terminated task.
+ ****************************************************************************/
+
+static void _free_stack(TCB* tcb)
+{
+    /* The highest task-number is on the top of the stack.
+     * It can simply be freed. We also free the top of 
+     * the free-list, if this is the next highest task-number. 
+     */
+    
+    if (tcb->pid == lastpid) {
+        stack += tcb->stsize;
+        lastpid--;
+        while (fl_head->pid == lastpid) {
+            stack += fl_head->stsize;
+            lastpid--;
+            terminated--;
+            fl_head = fl_head->next; 
+        }    
+    }
+    else {
+        /* The free-list is ordered by task-number. The highest 
+         * number is first (corresponds to the top of the stack 
+         */
+        terminated++;
+        tcb->next = NULL;
+        if (fl_head == NULL) 
+            fl_head = tcb;
+        else {
+           register TCB* p, *prev;
+           p = prev = fl_head;
+           while (p != NULL && p->pid > tcb->pid) {
+               p = p->next; 
+               prev = prev->next;
+           }
+           tcb->next = p;
+           if (p == prev)
+              fl_head = tcb;
+           else
+              prev->next = tcb;
+        }
+    }       
+}
+
 
 
 /****************************************************************************
