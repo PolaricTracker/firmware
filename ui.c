1c72fb74a57bf32822f10f1fbde968b2b8993e36
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2009-09-29 12:55:33 +0000

    Some tidying + try to save power

--- ui.c
+++ ui.c
@@ -23,16 +23,23 @@
 #include "usb.h"
 
 
-static bool usb_on = false;
-static bool buzzer = false;
+static bool   usb_on = false;
+static bool   buzzer = false;
+static Mutex  beep_mutex;
+static float  _batt_voltage;
+static bool   _batt_charged = false;
 
+static bool _powerdown = false;  /* True if requested to go to power-off */
+static bool asleep     = false;  /* True if device is actually put into power-off mode */
+bool autopower  __attribute__ ((section (".noinit")));
+                                 /* True if we want device to be "turned on" by external charger */
+                                 
 static void ui_thread(void);
 static void batt_check_thread(void);
 static void wakeup_handler(void);
+static void enable_ports_offmode();
 
-static float _batt_voltage;
-static bool _batt_charged = false;
-static Mutex beep_mutex;
+
 
 
 void ui_init()
@@ -40,31 +47,19 @@ void ui_init()
       /* Enable wdt */
       wdt_enable(WDTO_4S);
       
-      /* DDR Registers */
-      make_output(LED1);
-      make_output(LED2);
-      make_output(LED3);
-      clear_port(LED1); 
-      clear_port(LED2);
-      make_output(LED4);
-      make_output(LED5);
       make_output(BUZZER);     
       rgb_led_off();
       clear_port(BUZZER);
       usb_on = false;
       mutex_init(&beep_mutex);
-            
-      /* Button */
-      make_input(BUTTON);
+
       EICRA = (1<<ISC10);
-      EIMSK = (1<<INT1);     
-       
-      /* Battery charging, etc. */
-      make_output(HIGH_CHARGE);
-      clear_port(HIGH_CHARGE);  
+      EIMSK = (1<<INT1);
+
+      make_input(BUTTON);
       make_input(EXT_CHARGER); 
       clear_port(EXT_CHARGER); /* No internal pull-up */
-      
+      enable_ports_offmode();  
       THREAD_START(ui_thread, STACK_LED); 
       THREAD_START(batt_check_thread, STACK_BATT); 
 }
@@ -72,7 +67,28 @@ void ui_init()
 
 
 /*************************************************************************
- * Handler for on/off button
+ * Port to be enabled, when MCU wakes up for charging.
+ *************************************************************************/
+ 
+static void enable_ports_offmode()
+{
+      make_output(HIGH_CHARGE);
+      clear_port(HIGH_CHARGE);  
+      make_output(LED1);
+      make_output(LED2);
+      make_output(LED3);
+      make_output(LED4);
+      make_output(LED5);
+      clear_port(LED1); 
+      clear_port(LED2);
+      rgb_led_off();
+}
+
+
+
+
+/*************************************************************************
+ * Handler routines for on/off pushbutton
  *************************************************************************/
  
 #define ONOFF_TIME 200
@@ -82,94 +98,6 @@ static Timer onoff_timer, push_timer;
 bool is_off = false;
 static void onoff_handler(void*);
 static void sleepmode(void);
-
-
-void turn_off()
-{
-    is_off = true; 
-    sleepmode();
-}
-
-
-/* 
- * Called by batt_thread to handle situations where device is waked up
- * (but not necessarily turned on) by external power suppply. 
- */
-bool autopower  __attribute__ ((section (".noinit")));
-static void wakeup_handler()
-{
-   if (is_off && (pin_is_high(EXT_CHARGER) || usb_on || usb_con())) {
-       wdt_enable(WDTO_4S);
-       if(GET_BYTE_PARAM(AUTOPOWER)) {   
-          is_off = false;
-          autopower = true; /* Turn off if ext pwr is removed */
-          soft_reset(); 
-       }
-   }    
-   /* if (is_off && gps_hasWaiters())
-        gps_on(); */
-}
-
-
-
-static void onoff_handler(void* x)
-{
-    if (is_off) {
-       is_off = false;
-       autopower = false;
-       soft_reset(); 
-    }
-    else 
-       turn_off();
-}  
-
-
-
-/*
- * The last thing to be done before putting CPU to sleep:
- * Power down (almost) everything
- */
-static bool _powerdown = false;
-void powerdown_handler()
-{
-     if (!_powerdown)
-         return;
-    
-     _powerdown = false;
-    
-     EIMSK = (1<<INT1); 
-    /*
-     * Use pin-change interrupt to wake up the device if 
-     * external charger is plugged in. We do not need an ISR for this
-     * We just assume that one exists
-     */
-     set_bit (PCMSK0, PCINT6);
-     set_bit (PCICR, PCIE0); 
-    
-    /* External devices should be turned off. Todo: USB. 
-     * Note that after this, the device should be reset when it is re-activated */
-     autopower = false;
-     adf7021_power_off();
-     gps_off();
-     clear_port(LED1);
-     clear_port(LED2);
-     rgb_led_off();
-     wdt_disable();
-}
-
-
-
-static void sleepmode()
-{
-    if (is_off && !pin_is_high(EXT_CHARGER) && !usb_on && !usb_con() && pin_is_high(BUTTON)) {
-        _powerdown = true;
-        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-    }
-    else
-        set_sleep_mode(SLEEP_MODE_IDLE);        
-}
-
-
 
 static uint8_t tmp_push_count = 0, push_count = 0;
 static void push_thandler(void* x)
@@ -206,6 +134,32 @@ ISR(INT1_vect)
     }
 }
 
+
+/*
+ * Called when pushbutton is pressed for more than 2 seconds.
+ * Turn off or on
+ */
+ 
+static void onoff_handler(void* x)
+{
+    if (is_off) {
+       is_off = false;
+       autopower = false;
+       soft_reset(); 
+    }
+    else 
+       turn_off();
+}  
+
+
+
+/************************************************************************
+ * Execute pushbutton commands
+ *  3 times - send aprs report
+ *  4 times - add (and send) object report
+ *  5 times - delete all objects. 
+ ************************************************************************/
+ 
 void tracker_addObject();
 void tracker_clearObjects();
 
@@ -226,27 +180,109 @@ void push_handler()
         beeps("..-. -.-.");
         tracker_clearObjects();
     }
-
     push_count = 0;
-    
 }
 
 
-/************************************************************************
- * To be called from system clock ISR
- * Rate: 2400 Hz
- ************************************************************************/
 
-void ui_clock()
+/***************************************************************************** 
+ * Called by batt_thread to handle situations where device is waked up
+ * (but not necessarily turned on) by external power suppply. 
+ *****************************************************************************/
+ 
+static void wakeup_handler()
 {
-   if (buzzer)
-      toggle_port(BUZZER);       
+   if (asleep && is_off && (pin_is_high(EXT_CHARGER) || usb_on || usb_con())) {
+       wdt_enable(WDTO_4S);
+       asleep = false;
+       enable_ports_offmode();
+       if(GET_BYTE_PARAM(AUTOPOWER)) {   
+          is_off = false;
+          autopower = true; /* Turn off if ext pwr is removed */
+          soft_reset(); 
+       }
+   }    
+}
+
+/****************************************************************************
+ * Routines to turn off the device.
+ * 
+ ****************************************************************************/
+
+void turn_off()
+{
+    is_off = true; 
+    sleepmode();
+}
+
+
+/*
+ * The last thing to be done before putting CPU to sleep:
+ * Power down (almost) everything. Called from root thread (see main.c)
+ */
+void powerdown_handler()
+{
+     if (!_powerdown)
+         return;
+    
+     _powerdown = false;
+    
+     EIMSK = (1<<INT1); 
+    /*
+     * Use pin-change interrupt to wake up the device if 
+     * external charger is plugged in. We do not need an ISR for this
+     * We just assume that one exists
+     */
+     set_bit (PCMSK0, PCINT6);
+     set_bit (PCICR, PCIE0); 
+    
+    /* External devices should be turned off. Todo: USB. 
+     * Note that after this, the device should be reset when it is re-activated */
+     autopower = false;
+     adf7021_power_off();
+     gps_off();
+     clear_port(LED1);
+     clear_port(LED2);
+     rgb_led_off();
+
+     DDRA = 0;
+     PORTA = 0;
+     DDRC = 0;
+     PORTC = 0;
+     DDRD = 0;
+     PORTD = 0;
+     DDRF = 0;
+     PORTF = 0;
+     make_input(EXT_CHARGER);
+     clear_port(EXT_CHARGER); /* No internal pull-up */
+     make_input(BUTTON);
+     asleep = true;
+     wdt_disable();
+}
+
+
+/*
+ * Turn off device when is_off is true and not connected to charger.
+ * Called periodically from batt_check_thread (see below)
+ */
+static void sleepmode()
+{
+    if (is_off && !pin_is_high(EXT_CHARGER) && !usb_on && !usb_con() && pin_is_high(BUTTON)) {
+        _powerdown = true;
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+        /* powerdown_handler will be called as the last thing */
+    }
+    else
+        set_sleep_mode(SLEEP_MODE_IDLE);        
 }
 
 
 
+
+
+
 /************************************************************************
- * 1200 Hz BEEP
+ * 1200 Hz BEEP (sound signal)
  * t - duration in 10ms units
  * s - A sequence of dashes '-' and dots '.' (morse code). 
  *     Space means pause
@@ -261,6 +297,10 @@ static void _beep(uint16_t t)
 }
   
 
+/*
+ * Special version, to be called from root thread before going to poweroff-mode
+ * cannot block here. Use spinlock instead.
+ */
 void lbeep()
 {
     static bool beeped;
@@ -273,6 +313,8 @@ void lbeep()
     beeped = true;
 }
 
+
+/* Beep for 10 * t ms */
 void beep(uint16_t t)
 {
     mutex_lock(&beep_mutex);
@@ -280,6 +322,8 @@ void beep(uint16_t t)
     mutex_unlock(&beep_mutex);
 }
 
+
+/* Morse code */
 void beeps(char* s)
 {
     mutex_lock(&beep_mutex);
@@ -297,8 +341,21 @@ void beeps(char* s)
 }
 
 
+/*
+ * To be called from system clock ISR to generate sound
+ * Rate: 2400 Hz
+ */
+
+void ui_clock()
+{
+   if (buzzer)
+      toggle_port(BUZZER);       
+}
+
+
 void beep_lock()    {mutex_lock(&beep_mutex);}
 void beep_unlock()  {mutex_unlock(&beep_mutex);}
+
 
 
 /*************************************************************************
@@ -407,15 +464,14 @@ static void ui_thread(void)
     clear_port(LED1);    
     clear_port(LED2);
     rgb_led_off();
-    
+
+    /* 'QRV' in morse code */
     beeps("--.- .-. ...-");   
     if (usb_on)
         rgb_led_on(false,false,true);
    
     BLINK_NORMAL;
     while (1) {
-        /* Reset WDT */
-        wdt_reset(); 
         set_port( LED1 );
         sleep(blink_length);
         clear_port( LED1 );
@@ -426,7 +482,8 @@ static void ui_thread(void)
  
 
 /**********************************************************************
- * Battery stuff
+ * Thread that mainly controls charging of battery.
+ * It also resets WDT and runs pushbutton commands (including turn-off) 
  **********************************************************************/
 
 
@@ -447,7 +504,10 @@ static void batt_check_thread()
        _batt_voltage += adc_get(ADC_CHANNEL_0) * ADC_VBATT_DIVIDE;
        _batt_voltage /= 2;
        adc_disable();
-    
+
+       /* If battery is fully charged, charge current should be low. If not, it should
+        * be high
+        */
        if (_batt_voltage >= BATT_HIGHCHARGE_MAX && !_batt_charged) {
           _batt_charged = true;
           clear_port(HIGH_CHARGE);
@@ -457,7 +517,8 @@ static void batt_check_thread()
              _batt_charged = false;
              set_port(HIGH_CHARGE);
           }
-       
+
+       /* Warn or turn off device if battery voltage is too low */
        if (_batt_voltage <= BATT_LOW_WARNING) {
           if (_batt_voltage <= BATT_LOW_TURNOFF)
              turn_off();
@@ -474,7 +535,8 @@ static void batt_check_thread()
           }   
        }
         
-       /* External charger handler. Indicate when plugged in
+       /*
+        * External charger handler. Indicate when plugged in
         * even if device is "turned off" 
         */   
        make_output(EXT_CHARGER); 
@@ -484,11 +546,11 @@ static void batt_check_thread()
        clear_port(EXT_CHARGER);
        sleep(5);
  
-       if (((pin_is_high(EXT_CHARGER) /* && _batt_voltage >= 6.75 */ )  || usb_con()) && !usb_on) {
+       if (((pin_is_high(EXT_CHARGER) )  || usb_con()) && !usb_on) {
           if (_batt_charged) 
-             rgb_led_on(false,true,false);
+             rgb_led_on(false,true,false); /* Green if fully charged */
           else
-             rgb_led_on(true,false,false);
+             rgb_led_on(true,false,false); /* Red if still charging */
        }
        else {
           /* If only USB is connected, go briefly to red every 3rd round, 
@@ -502,12 +564,15 @@ static void batt_check_thread()
             turn_off();
          sleep(10);
          led_usb_restore();   
+         /* Turn off device if told to */
          sleepmode();
        }   
        
        sleep(100);
        /* Things to do if waked up by external charger */
        wakeup_handler();
+      /* Reset WDT */
+       wdt_reset();
        
        if (push_count > 0)
           push_handler();
