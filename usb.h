575b7a125b6a4e541a9c1ae29abdb674080f0c1f
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2012-12-01 00:27:07 +0000

    Newer version of LUFA

--- usb.h
+++ usb.h
@@ -1,70 +1,63 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2010.
+              
+  dean [at] fourwalledcubicle [dot] com
+      www.fourwalledcubicle.com
+*/
 
-#ifndef _CDC_H_
-#define _CDC_H_
-      
+/*
+  Copyright 2010  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+
+  Permission to use, copy, modify, distribute, and sell this 
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in 
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting 
+  documentation, and that the name of the author not be used in 
+  advertising or publicity pertaining to distribution of the 
+  software without specific, written prior permission.
+
+  The author disclaim all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/** \file
+ *
+ *  Header file for USBtoSerial.c.
+ */
+
+#ifndef _USB_SERIAL_H_
+#define _USB_SERIAL_H_
+
 	/* Includes: */
 		#include <avr/io.h>
-		#include <avr/pgmspace.h>
+		#include <avr/wdt.h>
+		#include <avr/interrupt.h>
+		#include <avr/power.h>
 
 		#include "usb_descriptors.h"
-		#include <USB.h>                // USB Functionality
 
-
-	/* Macros: */
-		#define GET_LINE_CODING              0x21
-		#define SET_LINE_CODING              0x20
-		#define SET_CONTROL_LINE_STATE       0x22
+		#include <LUFA/Version.h>
+		#include <LUFA/Drivers/USB/USB.h>
+		#include <LUFA/Drivers/USB/Class/CDC.h>
 
 	/* Macros: */
-		/** CDC Class specific request to get the current virtual serial port configuration settings. */
-		#define REQ_GetLineEncoding          0x21
+				
+	/* Function Prototypes: */
+		void SetupHardware(void);
 
-		/** CDC Class specific request to set the current virtual serial port configuration settings. */
-		#define REQ_SetLineEncoding          0x20
-
-		/** CDC Class specific request to set the current virtual serial port handshake line states. */
-		#define REQ_SetControlLineState      0x22
+		void EVENT_USB_Device_Connect(void);
+		void EVENT_USB_Device_Disconnect(void);
+		void EVENT_USB_Device_ConfigurationChanged(void);
+		void EVENT_USB_Device_UnhandledControlRequest(void);
 		
+		void EVENT_CDC_Device_LineEncodingChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo);
 
-
-
-	/* Event Handlers: */
-      HANDLES_EVENT(USB_Reset);
-		HANDLES_EVENT(USB_Connect);
-		HANDLES_EVENT(USB_Disconnect);
-		HANDLES_EVENT(USB_ConfigurationChanged);
-		HANDLES_EVENT(USB_UnhandledControlPacket);
-		
-	/* Type Defines: */
-		typedef struct
-		{
-			uint32_t BaudRateBPS;
-			uint8_t  CharFormat;
-			uint8_t  ParityType;
-			uint8_t  DataBits;
-		} CDC_Line_Coding_t;
-		
-	/* Enums: */
-		enum
-		{
-			OneStopBit          = 0,
-			OneAndAHalfStopBits = 1,
-			TwoStopBits         = 2,
-		} CDC_Line_Coding_Format;
-		
-		enum
-		{
-			Parity_None         = 0,
-			Parity_Odd          = 1,
-			Parity_Even         = 2,
-			Parity_Mark         = 3,
-			Parity_Space        = 4,
-		} CDC_Line_Codeing_Parity;
-
-
-   /* Function prototypes: */
-      void usb_init(void);
-      void USB_USBTask(void);
-      bool usb_con(void);
-      
 #endif
