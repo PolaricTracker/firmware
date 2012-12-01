494184c48bbe778e0093b1b8822d92e05b961c63
la7eca <la7eca@cac26f7a-a842-4ca4-81b3-0ef4ff02721b>
2012-12-01 00:17:11 +0000

    Many changes

--- fbuf.c
+++ fbuf.c
@@ -25,15 +25,19 @@ uint8_t   _fbuf_length[FBUF_SLOTS];
 uint8_t   _fbuf_next  [FBUF_SLOTS];
 char      _fbuf_buf   [FBUF_SLOTS][FBUF_SLOTSIZE]; 
 
+static uint8_t _split(uint8_t islot, uint8_t pos);
+
 
 /*
  * Note: We assume that FBUF objects are not shared between 
  * ISRs/threads. 
  * 
- * We also assume that slots are not shared between FBUF objects, 
- * and that FBUF objects are not accessed from interrupt handlers. 
+ * We also assume that FBUF objects are not accessed from interrupt handlers. 
  * That may change later. In that case, check if we need to protect
- * parts of code by disabling interrupts.
+ * parts of code by disabling interrupts. slots may be shared between
+ * FBUF objects but it is problematic and this should be tested and 
+ * analysed more. We should disallow writing to a FBUF that contains
+ * shared slots.
  */
 
 
@@ -62,7 +66,7 @@ static uint8_t _fbuf_newslot ()
  
 void fbuf_new (FBUF* bb)
 {
-    bb->head = bb->wslot = bb->rslot = NILPTR; 
+    bb->head = bb->wslot = bb->rslot = _fbuf_newslot();
     bb->rpos = 0;
     bb->length = 0;
 }
@@ -80,9 +84,30 @@ void fbuf_release(FBUF* bb)
        _fbuf_refcnt[b]--; 
        b = _fbuf_next[b]; 
     } 
-    fbuf_new(bb);
+    bb->head = bb->wslot = bb->rslot = NILPTR;
+    bb->rpos = bb->length = 0;
 }
 
+
+/*******************************************************
+ *   Create a new reference to a buffer chain
+ *******************************************************/
+
+FBUF fbuf_newRef(FBUF* bb)
+{
+  FBUF newb;
+  register uint8_t b = bb->head;
+  while (b != NILPTR) 
+  {
+    _fbuf_refcnt[b]++; 
+    b = _fbuf_next[b]; 
+  } 
+  newb.head = bb->head; 
+  newb.length = bb->length; 
+  fbuf_reset(&newb);
+  newb.wslot = bb->wslot;
+  return newb;
+}
 
 
 /*******************************************************
@@ -118,6 +143,10 @@ void fbuf_rseek(FBUF* b, const uint8_t pos)
  
 void fbuf_putChar (FBUF* b, const char c)
 {
+    /* if wslot is NIL it means that writing is not allowed */
+    if (b->wslot == NILPTR)
+       return;
+    
     register uint8_t pos = _fbuf_length[b->wslot]; 
     if (pos == FBUF_SLOTSIZE || b->head == NILPTR)
     {
@@ -135,13 +164,17 @@ void fbuf_putChar (FBUF* b, const char c)
 }
 
 
-
+    
 
 /*******************************************************
- * Merge a buffer chain into another buffer chain
+ * Insert a buffer chain x into another buffer chain b 
+ * at position pos
+ * 
+ * Note: After calling this, writing into the buffers
+ * are disallowed.
  *******************************************************/
  
-void fbuf_merge(FBUF* b, FBUF* x, uint8_t pos)
+void fbuf_insert(FBUF* b, FBUF* x, uint8_t pos)
 {
     register uint8_t islot = b->head;    
     while (pos >= FBUF_SLOTSIZE) {
@@ -158,21 +191,64 @@ void fbuf_merge(FBUF* b, FBUF* x, uint8_t pos)
         _fbuf_refcnt[xlast]++;
     }
     
+    /* Insert x chain after islot */  
+    _fbuf_next[xlast] = _split(islot, pos); 
+    _fbuf_next[islot] = x->head;
     
-    /* Split slot if necessary */    
-    if (pos!=0) {
-         register uint8_t newslot = _fbuf_newslot();
-         _fbuf_next[newslot] = _fbuf_next[islot];
-         _fbuf_next[islot] = newslot;
-         for (uint8_t i = 0; i<FBUF_SLOTSIZE-pos; i++)
-             _fbuf_buf[newslot][i] = _fbuf_buf[islot][pos+i];   
-         _fbuf_length[newslot] = FBUF_SLOTSIZE - pos;
-         _fbuf_length[islot] = pos;               
+    b->wslot = x->wslot = NILPTR; // Disallow writing
+    b->length += x->length;
+}
+
+
+/*****************************************************
+ * Connect b buffer to x, at position pos. In practice
+ * this mean that we get two buffers, with different
+ * headers but with a shared last part. 
+ * 
+ * Note: After calling this, writing into the buffers
+ * are not allowed. 
+ *****************************************************/
+
+void fbuf_connect(FBUF* b, FBUF* x, uint8_t pos)
+{
+    register uint8_t islot = x->head;  
+    register p = pos;
+    while (p >= FBUF_SLOTSIZE) {
+        p -= _fbuf_length[islot]; 
+        if (p > 0) 
+            islot = _fbuf_next[islot];
     }
     
-    /* Insert x chain after islot */  
-    _fbuf_next[xlast] = _fbuf_next[islot];
-    _fbuf_next[islot] = x->head;
+    /* Find last slot of b and connect it to rest of x */
+    register uint8_t xlast = b->head;
+    while (_fbuf_next[xlast] != NILPTR) 
+        xlast = _fbuf_next[xlast];
+    _fbuf_next[xlast] = _split(islot, p);
+    
+    /* Increment reference count of rest of x */
+    while (_fbuf_next[xlast] != NILPTR) {
+        xlast = _fbuf_next[xlast];
+        _fbuf_refcnt[xlast]++;
+    }
+    
+    b->wslot = x->wslot = NILPTR; // Disallow writing
+    b->length = b->length + x->length - pos;
+}
+
+
+
+static uint8_t _split(uint8_t islot, uint8_t pos)
+{
+      if (pos == 0)
+          return _fbuf_next[islot];
+      register uint8_t newslot = _fbuf_newslot();
+      _fbuf_next[newslot] = _fbuf_next[islot];
+      _fbuf_next[islot] = newslot;
+      for (uint8_t i = 0; i<FBUF_SLOTSIZE-pos; i++)
+          _fbuf_buf[newslot][i] = _fbuf_buf[islot][pos+i];   
+      _fbuf_length[newslot] = FBUF_SLOTSIZE - pos;
+      _fbuf_length[islot] = pos; 
+      return newslot;
 }
 
 
@@ -211,11 +287,11 @@ void fbuf_putstr(FBUF* b, const char *data)
 void fbuf_putstr_P(FBUF *b, const char * data) 
 {
    char c;
-	while ((c = pgm_read_byte(data)) != 0) 
-   { 	
-		fbuf_putChar(b, pgm_read_byte(data) );
+   while ((c = pgm_read_byte(data)) != 0) 
+   { 
+      fbuf_putChar(b, pgm_read_byte(data) );
       data++;
-   }	
+   }
 }
 
 
@@ -247,6 +323,8 @@ char fbuf_getChar(FBUF* b)
 char* fbuf_read (FBUF* b, uint8_t size, char *buf)
 {
     register uint8_t n, bb, r=0;
+    if (b->length < size)
+       size = b->length; 
     bb = b->head; 
     while ( bb != NILPTR )
     {
@@ -257,9 +335,10 @@ char* fbuf_read (FBUF* b, uint8_t size, char *buf)
        strncpy(buf+r, _fbuf_buf[bb], n);
        r += n; 
        bb = _fbuf_next[bb];
-       if (r >= size || r >= b->length)
+       if (r >= size ) 
            break;
     }
+    buf[r] = '\0';
     return buf; 
 }
 
